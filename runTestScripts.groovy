d818a2060d5cad7234e76050468c603b0b37930d
Ginny Guan <ginny@iotechsys.com>
2020-09-09 17:26:36 +0800

    Add V2 API app-service test to run on Jenkins
    Fix https://github.com/edgexfoundry/edgex-taf-pipelines/issues/40
    
    Signed-off-by: Ginny Guan <ginny@iotechsys.com>

--- runTestScripts.groovy
+++ runTestScripts.groovy
@@ -69,6 +69,19 @@ def main() {
                         }
                     }
                 }
+                stage ("Run V2 API Tests - ${ARCH}${USE_DB}${USE_SECURITY}${BRANCH}"){
+                    echo "===== Run V2 API Tests ====="
+                    sh "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:rw,z -w ${env.WORKSPACE} \
+                            -e COMPOSE_IMAGE=${COMPOSE_IMAGE} -e SECURITY_SERVICE_NEEDED=${SECURITY_SERVICE_NEEDED} \
+                            -e ARCH=${ARCH} --security-opt label:disable \
+                            -v /var/run/docker.sock:/var/run/docker.sock ${TAF_COMMOM_IMAGE} \
+                            --exclude Skipped --include v2-api -u functionalTest/V2-API -p default"
+
+                    dir ('TAF/testArtifacts/reports/rename-report') {
+                        sh "cp ../edgex/log.html v2-api-log.html"
+                        sh "cp ../edgex/report.xml v2-api-report.xml"
+                    }
+                }
                 stage ("Stash Report - ${ARCH}${USE_DB}${USE_SECURITY}${BRANCH}") {
                     echo '===== Merge Reports ====='
                     sh "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:rw,z -w ${env.WORKSPACE} \
