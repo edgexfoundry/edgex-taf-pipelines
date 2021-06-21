d4d268736ea878d66ed05ada29f12d7bfd94b2fb
Cherry Wang <cherry@iotechsys.com>
2021-06-21 09:11:39 +0800

    [functional-test] Remove unnecessary command for debug
    Signed-off-by: Cherry Wang <cherry@iotechsys.com>

--- runTestScripts.groovy
+++ runTestScripts.groovy
@@ -27,7 +27,6 @@ def main() {
                 stage ("Deploy EdgeX - ${ARCH}${USE_DB}${USE_SECURITY}${BRANCH}") {
                     dir ('TAF/utils/scripts/docker') {
                         sh "sh get-compose-file.sh ${USE_DB} ${ARCH} ${USE_SECURITY}"
-                        sh 'ls *.yml *.yaml'
                     }
 
                     sh "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:rw,z -w ${env.WORKSPACE} \
