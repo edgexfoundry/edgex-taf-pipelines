d9912075f98efef9a65481d90b278a5d1c512825
Ginny Guan <ginny@iotechsys.com>
2021-07-07 11:17:33 +0800

    [functional-test] Change repo master branch to main
    Also remove BRANCHLIST
    
    Signed-off-by: Ginny Guan <ginny@iotechsys.com>

--- runTestScripts.groovy
+++ runTestScripts.groovy
@@ -1,104 +1,100 @@
 
 def main() {
-    def BRANCHES = "${BRANCHLIST}".split(',')
     def PROFILES = "${PROFILELIST}".split(',')
     def USE_SECURITY = '-'
     def runbranchstage = [:]
+        
+    if ("${SECURITY_SERVICE_NEEDED}" == 'true') {
+        USE_SECURITY = '-security-'
+    }
 
-    for (x in BRANCHES) {
-        
-        if ("${SECURITY_SERVICE_NEEDED}" == 'true') {
-            USE_SECURITY = '-security-'
-        } 
-        
-        def BRANCH = x
-        runbranchstage["Test ${ARCH}${USE_DB}${USE_SECURITY}${BRANCH}"]= {
-            node("${NODE}") {
-                stage ('Checkout edgex-taf repository') {
-                    checkout([$class: 'GitSCM',
-                        branches: [[name: "refs/${TAF_BRANCH}"]],
-                        doGenerateSubmoduleConfigurations: false, 
-                        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: '']], 
-                        submoduleCfg: [], 
-                        userRemoteConfigs: [[url: 'https://github.com/edgexfoundry/edgex-taf.git']]
-                        ])
+    runbranchstage["Test ${ARCH}${USE_DB}${USE_SECURITY}${TAF_BRANCH}"]= {
+        node("${NODE}") {
+            stage ('Checkout edgex-taf repository') {
+                checkout([$class: 'GitSCM',
+                    branches: [[name: "refs/${TAF_BRANCH}"]],
+                    doGenerateSubmoduleConfigurations: false,
+                    extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: '']],
+                    submoduleCfg: [],
+                    userRemoteConfigs: [[url: 'https://github.com/edgexfoundry/edgex-taf.git']]
+                ])
+            }
+
+            stage ("Deploy EdgeX - ${ARCH}${USE_DB}${USE_SECURITY}${TAF_BRANCH}") {
+                dir ('TAF/utils/scripts/docker') {
+                    sh "sh get-compose-file.sh ${USE_DB} ${ARCH} ${USE_SECURITY} pre-release ${COMPOSE_BRANCH}"
                 }
 
-                stage ("Deploy EdgeX - ${ARCH}${USE_DB}${USE_SECURITY}${BRANCH}") {
-                    dir ('TAF/utils/scripts/docker') {
-                        sh "sh get-compose-file.sh ${USE_DB} ${ARCH} ${USE_SECURITY} pre-release ${COMPOSE_BRANCH}"
-                    }
-
-                    sh "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:rw,z -w ${env.WORKSPACE} \
-                            -e COMPOSE_IMAGE=${COMPOSE_IMAGE} -e SECURITY_SERVICE_NEEDED=${SECURITY_SERVICE_NEEDED} \
-                            -e USE_DB=${USE_DB} --security-opt label:disable \
-                            -v /var/run/docker.sock:/var/run/docker.sock ${TAF_COMMON_IMAGE} \
-                            --exclude Skipped --include deploy-base-service -u deploy.robot -p default"
+                sh "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:rw,z -w ${env.WORKSPACE} \
+                    -e COMPOSE_IMAGE=${COMPOSE_IMAGE} -e SECURITY_SERVICE_NEEDED=${SECURITY_SERVICE_NEEDED} \
+                    -e USE_DB=${USE_DB} --security-opt label:disable \
+                    -v /var/run/docker.sock:/var/run/docker.sock ${TAF_COMMON_IMAGE} \
+                    --exclude Skipped --include deploy-base-service -u deploy.robot -p default"
                 }
                 
-                stage ("Run V2 API Tests - ${ARCH}${USE_DB}${USE_SECURITY}${BRANCH}"){
-                    echo "===== Run V2 API Tests ====="
-                    sh "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:rw,z -w ${env.WORKSPACE} \
+            stage ("Run V2 API Tests - ${ARCH}${USE_DB}${USE_SECURITY}${TAF_BRANCH}"){
+                echo "===== Run V2 API Tests ====="
+                sh "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:rw,z -w ${env.WORKSPACE} \
+                    -e COMPOSE_IMAGE=${COMPOSE_IMAGE} -e SECURITY_SERVICE_NEEDED=${SECURITY_SERVICE_NEEDED} \
+                    -e ARCH=${ARCH}  --env-file ${env.WORKSPACE}/TAF/utils/scripts/docker/common-taf.env \
+                    --security-opt label:disable -v /var/run/docker.sock:/var/run/docker.sock ${TAF_COMMON_IMAGE} \
+                    --exclude Skipped --include v2-api -u functionalTest/V2-API -p default"
+
+                dir ('TAF/testArtifacts/reports/rename-report') {
+                    sh "cp ../edgex/log.html v2-api-log.html"
+                    sh "cp ../edgex/report.xml v2-api-report.xml"
+                }
+            }
+
+            echo "Profiles : ${PROFILES}"
+            stage ("Run Device Service Tests - ${ARCH}${USE_DB}${USE_SECURITY}${TAF_BRANCH}") {
+                script {
+                    for (y in PROFILES) {
+                        def profile = y
+                        echo "Profile : ${profile}"
+                        echo "===== Run ${profile} Test Case ====="
+                        sh "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:rw,z -w ${env.WORKSPACE} \
                             -e COMPOSE_IMAGE=${COMPOSE_IMAGE} -e SECURITY_SERVICE_NEEDED=${SECURITY_SERVICE_NEEDED} \
-                            -e ARCH=${ARCH}  --env-file ${env.WORKSPACE}/TAF/utils/scripts/docker/common-taf.env \
-                            --security-opt label:disable -v /var/run/docker.sock:/var/run/docker.sock ${TAF_COMMON_IMAGE} \
-                            --exclude Skipped --include v2-api -u functionalTest/V2-API -p default"
-
-                    dir ('TAF/testArtifacts/reports/rename-report') {
-                        sh "cp ../edgex/log.html v2-api-log.html"
-                        sh "cp ../edgex/report.xml v2-api-report.xml"
-                    }
-                }
-
-                echo "Profiles : ${PROFILES}"
-                stage ("Run Device Service Tests - ${ARCH}${USE_DB}${USE_SECURITY}${BRANCH}") {
-                    script {
-                        for (y in PROFILES) {
-                            def profile = y
-                            echo "Profile : ${profile}"
-                            echo "===== Run ${profile} Test Case ====="
-                            sh "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:rw,z -w ${env.WORKSPACE} \
-                                    -e COMPOSE_IMAGE=${COMPOSE_IMAGE} -e SECURITY_SERVICE_NEEDED=${SECURITY_SERVICE_NEEDED} \
-                                    -e ARCH=${ARCH} --security-opt label:disable \
-                                    -v /var/run/docker.sock:/var/run/docker.sock ${TAF_COMMON_IMAGE} \
-                                    --exclude Skipped -u functionalTest/device-service/common -p ${profile}"
-                            
-                            dir ('TAF/testArtifacts/reports/rename-report') {
-                                sh "cp ../edgex/log.html ${profile}-common-log.html"
-                                sh "cp ../edgex/report.xml ${profile}-common-report.xml"
-                            }
-                        }
-                    }
-                }
-
-                stage ("Stash Report - ${ARCH}${USE_DB}${USE_SECURITY}${BRANCH}") {
-                    echo '===== Merge Reports ====='
-                    sh "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:rw,z -w ${env.WORKSPACE} \
-                                -e COMPOSE_IMAGE=${COMPOSE_IMAGE} ${TAF_COMMON_IMAGE} \
-                                rebot --inputdir TAF/testArtifacts/reports/rename-report \
-                                --outputdir TAF/testArtifacts/reports/${BRANCH}-report"
-
-                    dir ("TAF/testArtifacts/reports/${BRANCH}-report") {
-                        // Check if the merged-report folder exists
-                        def mergeExist = sh (
-                            script: 'ls ../ | grep merged-report',
-                            returnStatus: true
-                        )
-                        if (mergeExist != 0) {
-                            sh 'mkdir ../merged-report'
-                        }
-                        //Copy log file to merged-report folder
-                        sh "cp log.html ../merged-report/${ARCH}${USE_DB}${USE_SECURITY}${BRANCH}-log.html"
-                        sh "cp result.xml ../merged-report/${ARCH}${USE_DB}${USE_SECURITY}${BRANCH}-report.xml"
-                    }
-                    stash name: "${ARCH}${USE_DB}${USE_SECURITY}${BRANCH}-report", includes: "TAF/testArtifacts/reports/merged-report/*", allowEmpty: true
-                }
-                stage ("Shutdown EdgeX - ${ARCH}${USE_DB}${USE_SECURITY}${BRANCH}") {
-                    sh "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:rw,z -w ${env.WORKSPACE} \
-                            -e COMPOSE_IMAGE=${COMPOSE_IMAGE} --security-opt label:disable \
+                            -e ARCH=${ARCH} --security-opt label:disable \
                             -v /var/run/docker.sock:/var/run/docker.sock ${TAF_COMMON_IMAGE} \
-                            --exclude Skipped --include shutdown-edgex -u shutdown.robot -p default"
+                            --exclude Skipped -u functionalTest/device-service/common -p ${profile}"
+                            
+                        dir ('TAF/testArtifacts/reports/rename-report') {
+                            sh "cp ../edgex/log.html ${profile}-common-log.html"
+                            sh "cp ../edgex/report.xml ${profile}-common-report.xml"
+                        }
+                    }
                 }
+            }
+
+            stage ("Stash Report - ${ARCH}${USE_DB}${USE_SECURITY}${TAF_BRANCH}") {
+                echo '===== Merge Reports ====='
+                sh "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:rw,z -w ${env.WORKSPACE} \
+                    -e COMPOSE_IMAGE=${COMPOSE_IMAGE} ${TAF_COMMON_IMAGE} \
+                    rebot --inputdir TAF/testArtifacts/reports/rename-report \
+                    --outputdir TAF/testArtifacts/reports/report"
+
+                dir ("TAF/testArtifacts/reports/report") {
+                    // Check if the merged-report folder exists
+                    def mergeExist = sh (
+                        script: 'ls ../ | grep merged-report',
+                        returnStatus: true
+                    )
+                    if (mergeExist != 0) {
+                        sh 'mkdir ../merged-report'
+                    }
+                    //Copy log file to merged-report folder
+                    sh "cp log.html ../merged-report/${ARCH}${USE_DB}${USE_SECURITY}log.html"
+                    sh "cp result.xml ../merged-report/${ARCH}${USE_DB}${USE_SECURITY}report.xml"
+                }
+                stash name: "${ARCH}${USE_DB}${USE_SECURITY}report", includes: "TAF/testArtifacts/reports/merged-report/*", allowEmpty: true
+            }
+
+            stage ("Shutdown EdgeX - ${ARCH}${USE_DB}${USE_SECURITY}${TAF_BRANCH}") {
+                sh "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:rw,z -w ${env.WORKSPACE} \
+                    -e COMPOSE_IMAGE=${COMPOSE_IMAGE} --security-opt label:disable \
+                    -v /var/run/docker.sock:/var/run/docker.sock ${TAF_COMMON_IMAGE} \
+                    --exclude Skipped --include shutdown-edgex -u shutdown.robot -p default"
             }
         }
     }
