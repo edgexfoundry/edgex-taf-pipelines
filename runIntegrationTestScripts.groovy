9302bd0f41ee6dda977fe730f84a63083be16e39
Cherry Wang <cherry@iotechsys.com>
2021-11-04 13:13:44 +0800

    test external mqtt trigger
    Signed-off-by: Cherry Wang <cherry@iotechsys.com>

--- runIntegrationTestScripts.groovy
+++ runIntegrationTestScripts.groovy
@@ -14,7 +14,7 @@ def main() {
                     doGenerateSubmoduleConfigurations: false,
                     extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: '']],
                     submoduleCfg: [],
-                    userRemoteConfigs: [[url: 'https://github.com/edgexfoundry/edgex-taf.git']]
+                    userRemoteConfigs: [[url: 'https://github.com/cherrycl/edgex-taf.git']]
                 ])
             }
             stage ("Retrieve Compose File - ${ARCH}${USE_SECURITY}${TAF_BRANCH}") {
